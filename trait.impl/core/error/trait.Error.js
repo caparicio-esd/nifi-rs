a937e6075b54dfc08e914fe72cb0e9bc85585042
caparicio-esd <caparicio-esd@users.noreply.github.com>
2025-11-09 10:57:54 +0000

    deploy: 0cfce8a5f39b39831c9bf164e229ed87d3889bff

--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["nifi_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"nifi_rs/common/client/enum.HttpClientError.html\" title=\"enum nifi_rs::common::client::HttpClientError\">HttpClientError</a>"]]]]);
+    var implementors = Object.fromEntries([["nifi_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"nifi_rs/common/client/enum.HttpClientError.html\" title=\"enum nifi_rs::common::client::HttpClientError\">HttpClientError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"nifi_rs/api/error/struct.ConversionError.html\" title=\"struct nifi_rs::api::error::ConversionError\">ConversionError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[311]}
+//{"start":57,"fragment_lengths":[607]}
